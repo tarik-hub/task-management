b3abc6110d812a4fcc4754fb698a11c9fefc3531
kirat14 <moumini.tarik@gmail.com>
2024-07-02 17:21:28 -0400

    Implement TaskService for business logic

---
+++ src/main/java/com/tarik/task_management/services/TaskService.java
@@ -0,0 +1,47 @@
+package com.tarik.task_management.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tarik.task_management.models.Task;
+import com.tarik.task_management.repositories.TaskRepository;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository){
+        this.taskRepository = taskRepository;
+    }
+
+    public Task createTask(Task task){
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task updateTask(Long id, Task taskDetails){
+        Task task = taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found"));
+        task.setTitle(taskDetails.getTitle());
+        task.setDescription(taskDetails.getDescription());
+        task.setDueDate(taskDetails.getDueDate());
+        task.setStatus(taskDetails.getStatus());
+
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        Task task = taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task Not found"));
+        taskRepository.delete(task);
+    }
+}
