a1e852807e58a5e7dab5a0d40c495d019a1cd243
tarik-hub <tarik.moumini@gmail.com>
2024-08-01 19:42:22 -0400

    Move frontend project under task-managment/frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CreateTask from './components/CreateTask';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Task Management App</h1>
+      <CreateTask />
+    </div>
+  );
+}
+
+export default App;
